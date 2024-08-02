aa61a888a50fceee595f0055909d2ff98cfd059d
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2024-08-02 13:44:55 -0700

    additional properties parsing WIP

--- src/r2x/parser/plexos.py
+++ src/r2x/parser/plexos.py
@@ -506,6 +506,7 @@ class PlexosParser(PCMParser):
         generator_fuel = self.db.query(fuel_query)
         generator_fuel_map = {key: value for key, value in generator_fuel}
 
+        system_generators.write_csv("system_generators.csv")
         # Iterate over properties for generator
         for generator_name, generator_data in system_generators.group_by("name"):
             generator_name = generator_name[0]
@@ -1081,6 +1082,7 @@ class PlexosParser(PCMParser):
             return time_series_data
         logger.warning("Property {} not supported. Skipping it.", property_name)
         logger.warning("Data file {} not supported yet. Skipping it.", relative_path)
+        # breakpoint()
         logger.warning("Columns not supported: {}", data_file.columns)
 
     def _retrieve_single_value_data(self, property_name, data_file):
