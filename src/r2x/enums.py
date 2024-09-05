ebce588a7cf1486133d2fe9a2baa4052387925a6
pesap <pesapsanchez@gmail.com>
2024-09-05 09:35:16 -0600

    fix(enums): Uppercase all enums

--- src/r2x/enums.py
+++ src/r2x/enums.py
@@ -1,21 +1,24 @@
-"""R2X enums."""
+"""Definition of enums used on R2X.
 
-from enum import auto, StrEnum
+In R2X most of the enums are `StrEnums` to be able to export it to csv easier.
+"""
+
+from enum import StrEnum
 
 
 class ReserveType(StrEnum):
     """Class representing different types of Reserves."""
 
-    SPINNING = auto()
-    FLEXIBILITY = auto()
-    REGULATION = auto()
+    SPINNING = "SPINNING"
+    FLEXIBILITY = "FLEXIBILITY"
+    REGULATION = "REGULATION"
 
 
 class ReserveDirection(StrEnum):
     """Class representing different Reserve Direction."""
 
-    UP = auto()
-    DOWN = auto()
+    UP = "UP"
+    DOWN = "DOWN"
 
 
 class ACBusTypes(StrEnum):
@@ -24,54 +27,54 @@ class ACBusTypes(StrEnum):
     For PCM translations, must of the buses are `PV`.
     """
 
-    PV = auto()
-    PQ = auto()
-    REF = auto()
+    PV = "PV"
+    PQ = "PQ"
+    REF = "REF"
 
 
 class PrimeMoversType(StrEnum):
     """EIA prime mover codes."""
 
-    BA = auto()
-    BT = auto()
-    CA = auto()
-    CC = auto()
-    CE = auto()
-    CP = auto()
-    CS = auto()
-    CT = auto()
-    ES = auto()
-    FC = auto()
-    FW = auto()
-    GT = auto()
-    HA = auto()
-    HB = auto()
-    HK = auto()
-    HY = auto()
-    IC = auto()
-    PS = auto()
-    OT = auto()
-    ST = auto()
-    PV = auto()
-    WT = auto()
-    WS = auto()
-    RTPV = auto()
+    BA = "BA"
+    BT = "BT"
+    CA = "CA"
+    CC = "CC"
+    CE = "CE"
+    CP = "CP"
+    CS = "CSV"
+    CT = "CT"
+    ES = "ES"
+    FC = "FC"
+    FW = "FW"
+    GT = "GT"
+    HA = "HA"
+    HB = "HB"
+    HK = "HK"
+    HY = "HY"
+    IC = "IC"
+    PS = "PS"
+    OT = "OT"
+    ST = "ST"
+    PV = "PV"
+    WT = "WT"
+    WS = "WS"
+    RTPV = "RTPV"
 
 
 class ThermalFuels(StrEnum):
     """Thermal fuels that reflect options in the EIA annual energy review."""
 
-    COAL = auto()
-    WASTE_COAL = auto()
-    DISTILLATE_FUEL_OIL = auto()
-    WASTE_OIL = auto()
-    PETROLEUM_COKE = auto()
-    RESIDUAL_FUEL_OIL = auto()
-    NATURAL_GAS = auto()
-    OTHER_GAS = auto()
-    NUCLEAR = auto()
-    AG_BIPRODUCT = auto()
-    MUNICIPAL_WASTE = auto()
-    WOOD_WASTE = auto()
-    GEOTHERMAL = auto()
-    OTHER = auto()
+    COAL = "COAL"
+    WASTE_COAL = "WASTE_COAL"
+    DISTILLATE_FUEL_OIL = "DISTILLATE_FUEL_OIL"
+    WASTE_OIL = "WASTE_OIL"
+    PETROLEUM_COKE = "PETROLEUM_COKE"
+    RESIDUAL_FUEL_OIL = "RESIDUAL_FUEL_OIL"
+    NATURAL_GAS = "NATURAL_GAS"
+    OTHER_GAS = "OTHER_GAS"
+    NUCLEAR = "NUCLEAR"
+    AG_BIOPRODUCT = "AG_BIOPRODUCT"
+    MUNICIPAL_WASTE = "MUNICIPAL_WASTE"
+    WOOD_WASTE = "WOOD_WASTE"
+    GEOTHERMAL = "GEOTHERMAL"
+    OTHER = "OTHER"
