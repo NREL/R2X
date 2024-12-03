31dccdf067c02acce16470451dd4acf55d72409f
zack jensen <zjensen@nrel.gov>
2024-12-03 09:48:20 -0700

    fix: resotring cli

--- src/r2x/cli.py
+++ src/r2x/cli.py
@@ -17,12 +17,6 @@ def cli():
         parser.print_help(sys.stderr)
         sys.exit(0)
 
-    if len(sys.argv) == 0:
-        a = 3
-        b = 5
-        c = a + b
-        print(c)
-
     args, remaining_args = parser.parse_known_args()
 
     if "--help" in remaining_args:
