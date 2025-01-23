98d3802daac57761f103877656f6228f7666ae3d
pesap <pesapsanchez@gmail.com>
2025-01-22 20:57:25 -0700

    test: Adding serialization test

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -33,6 +33,23 @@ def test_runner_exceptions(tmp_path, reeds_data_folder):
         _ = run(cli_input, {})
 
 
+def test_runner_serialization(tmp_path, reeds_data_folder):
+    cli_input = {
+        "name": "Test",
+        "weather_year": 2015,
+        "solve_year": [2055],
+        "input_model": "reeds-US",
+        "output_model": "sienna",
+        "output_folder": str(tmp_path),
+        "feature_flags": {"cool-feature": True},
+        "run_folder": reeds_data_folder,
+        "save": True,
+    }
+
+    _ = run(cli_input, {})
+    assert (tmp_path / f"{cli_input['name']}.json").exists()
+
+
 def test_init(tmp_path):
     cli_input = {"path": str(tmp_path)}
     _ = init(cli_input)
