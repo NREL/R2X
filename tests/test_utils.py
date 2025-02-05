2955fe5a7d66204c6674e2c40b07973f313080c9
pesap <pesapsanchez@gmail.com>
2025-02-05 10:06:08 -0700

    test: Adding testing for new features of `check_file_exists`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,11 @@
 import json
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 import pytest
 import yaml
 
-from r2x.utils import haskey, override_dict, read_user_dict
+from r2x.utils import check_file_exists, haskey, override_dict, read_user_dict
 
 
 @pytest.mark.utils
@@ -142,3 +144,15 @@ def test_read_user_dict(tmp_path):
 def test_update_dict(original, override, expected):
     result = override_dict(original, override)
     assert result == expected
+
+
+def test_check_file_exist():
+    csv_file_contents = "name,value\nTemp,25.5\nLoad,1200\nPressure,101.3"
+    with NamedTemporaryFile(mode="w+", suffix=".csv", delete=False) as temp_file:
+        temp_file.write(csv_file_contents)
+        temp_file.seek(0)
+
+        path = Path(temp_file.name)
+        folder = path.parent
+        fpath = check_file_exists(path.name, folder, folder=folder)
+        assert path == fpath
