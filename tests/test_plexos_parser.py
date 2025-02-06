b63dfcfac0a80f479f375151bdb1e5418465966b
ktehranchi <ktehranchi@stanford.edu>
2025-02-06 15:27:02 -0800

    sys variable test

--- tests/test_plexos_parser.py
+++ tests/test_plexos_parser.py
@@ -7,6 +7,7 @@ from r2x.config_scenario import Scenario
 from r2x.exceptions import ParserError
 from r2x.parser.handler import get_parser_data
 from r2x.parser.plexos import PlexosParser
+from r2x.models import Generator
 
 DB_NAME = "2-bus_example.xml"
 MODEL_NAME = "main_model"
@@ -40,6 +41,19 @@ def pjm_scenario(tmp_path, data_folder):
         user_dict={
             "fmap": {"xml_file": {"fname": "system.xml", "model_name": MODEL_NAME}},
         },
+    )
+
+
+@pytest.fixture
+def five_bus_variables_scenario(tmp_path, data_folder):
+    return Scenario.from_kwargs(
+        name="plexos_test",
+        input_model="plexos",
+        run_folder=data_folder / "5_bus_system_variables",
+        output_folder=tmp_path,
+        solve_year=2035,
+        weather_year=2012,
+        fmap={"xml_file": {"fname": "5_bus_system_variables.xml", "model": "default"}},
     )
 
 
@@ -83,3 +97,27 @@ def test_parser_system(pjm_scenario):
     # PJM system has 48 components
     total_components = sum(1 for _ in system.iter_all_components())
     assert total_components == 48
+
+
+def test_variable_parsing(five_bus_variables_scenario):
+    plexos_category_map = {
+        "thermal": {"fuel": "GAS", "type": "CC"},
+        "solar": {"fuel": "SUN", "type": "WT"},
+        "wind": {"fuel": "WIND", "type": "PV"},
+    }
+    five_bus_variables_scenario.defaults["plexos_category_map"] = plexos_category_map
+    five_bus_variables_scenario.model = "Base"
+    parser = get_parser_data(five_bus_variables_scenario, parser_class=PlexosParser)
+
+    system = parser.build_system()
+    assert isinstance(system, System)
+
+    record_ts = {}
+    for component in system.get_components(Generator):
+        if not system.has_time_series(component):
+            continue
+        ts_metadata = system.get_time_series(component, "max_active_power")
+        record_ts[component.name] = ts_metadata
+
+    assert sum(record_ts["SolarPV1"].data.to_pylist()) == 4224.0
+    assert "SolarPV2" not in record_ts
