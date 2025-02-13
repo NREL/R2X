6ad04f4ae25bcaad2acd4d5322a1bf6031ea7826
Micah Webb <105076850+micahpw-nrel@users.noreply.github.com>
2025-02-12 22:25:44 -0700

    Refactor plugins to use pluggy. Enable external plugins.

--- src/r2x/exporter/plexos.py
+++ src/r2x/exporter/plexos.py
@@ -2,7 +2,6 @@
 
 import string
 import uuid
-import pluggy
 from argparse import ArgumentParser
 from collections.abc import Callable
 from functools import partial
@@ -55,10 +54,6 @@ DEFAULT_XML_TEMPLATE = "master_9.2R6_btu.xml"
 EXT_PROPERTIES = {"UoS Charge", "Fixed Load"}
 KEYS_TO_FLATTEN = {"active_power_limits", "active_power_flow_limits", "storage_capacity"}
 
-hookimpl = pluggy.HookimplMarker("r2x_plugin")
-
-
-@hookimpl
 def cli_arguments(parser: ArgumentParser):
     """CLI arguments for the plugin."""
     parser.add_argument(
