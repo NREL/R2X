a0b28fffecc1527c9d4de434c12905f169d59ec3
Micah Webb <Webbmp91@gmail.com>
2025-03-10 09:40:21 -0600

    more progress

--- tests/mock_external_plugin/setup.py
+++ tests/mock_external_plugin/setup.py
@@ -4,5 +4,10 @@ setup(
     name="mock-r2x-external-plugin",
     version="0.1",
     packages=["module"],
-    entry_points={"r2x_plugin": ["mock_plugin = module.plugin"]},
+    entry_points={
+        "r2x_plugin": ["mock_plugin = module.plugin"],
+        "r2x_parser": ["mock_parser = module.plugin:TestExternalParser"],
+
+    },
+
 )
